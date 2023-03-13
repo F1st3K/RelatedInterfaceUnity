a74292165d7616de95927a27f864ff112e60df77
F1st3K <kostinnikita988@gmail.com>
2023-03-14 00:35:23 +0300

    changed objects

--- Assets/RelatedInterface/Scripts/ObjectMenu.cs
+++ Assets/RelatedInterface/Scripts/ObjectMenu.cs
@@ -1,20 +1,47 @@
-﻿using System;
-using TMPro;
-using TMPro.Examples;
+﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RelatedInterface.Scripts
 {
     public class ObjectMenu : MonoBehaviour
     {
-        [SerializeField] private GameObject gameObject;
+        [SerializeField] private GameObject someObject;
         
         private TMP_Text _nameObject;
+        private Toggle _selected;
+        private Toggle _showed;
+
+        public bool Selected => _selected.isOn;
 
         public void Awake()
         {
             _nameObject = GetComponentInChildren<TMP_Text>();
-            _nameObject.text = gameObject.name;
+            _showed = GetComponentInChildren<Toggle>();
+            _selected = GetComponentInChildren<Toggle>();
+            
+            _nameObject.text = someObject.name;
+            _showed.onValueChanged.AddListener(delegate {SwitchShow();});
+        }
+
+        public void SetAlpha(byte value)
+        {
+            
+        }
+
+        public void SetSelected(bool value)
+        {
+            
+        }
+
+        public void SetShowed(bool value)
+        {
+            
+        }
+
+        private void SwitchShow()
+        {
+            someObject.SetActive(_selected.isOn);
         }
     }
 }
