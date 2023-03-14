0486f0f3e2cbfe76899e431fe9610fde0f2d0402
F1st3K <kostinnikita988@gmail.com>
2023-03-14 17:26:40 +0300

    first working version

--- Assets/RelatedInterface/Scripts/Menu.cs
+++ Assets/RelatedInterface/Scripts/Menu.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,25 +6,92 @@ namespace RelatedInterface.Scripts
 {
     public class Menu : MonoBehaviour
     {
-        [SerializeField] private RectTransform rectTransform;
-        [SerializeField] private Toggle hideButton;
+        public List<ObjectMenu> _listObjects;
+        private RectTransform _rectTransform;
+        private Toggle _hideMenuToggle;
+        private Toggle _selectAllToggle;
+        private Toggle _hideObjectsToggle;
+        private Button _transparency10;
+        private Button _transparency30;
+        private Button _transparency50;
+        private Button _transparency75;
+        private Button _transparency100;
+        private GameObject _content;
+
 
         public void Awake()
         {
-            hideButton.onValueChanged.AddListener(delegate { SwitchShow();});
+            _listObjects = new List<ObjectMenu>();
+            GetAllComponents();
+            AddAllListeners();
+            foreach (Transform child in _content.transform)
+            {
+                if (child.TryGetComponent<ObjectMenu>(out var obj))
+                    _listObjects.Add(obj);
+            }
         }
 
-        public void Update()
+        private void GetAllComponents()
         {
-        
+            _rectTransform = GetComponent<RectTransform>();
+            var toggles = GetComponentsInChildren<Toggle>();
+            _hideMenuToggle = toggles[0];
+            _selectAllToggle = toggles[1];
+            _hideObjectsToggle = toggles[2];
+            var buttons = GetComponentsInChildren<Button>();
+            _transparency10 = buttons[0];
+            _transparency30 = buttons[1];
+            _transparency50 = buttons[2];
+            _transparency75 = buttons[3];
+            _transparency100 = buttons[4];
+            var scroll = GetComponentInChildren<ScrollRect>();
+            var viewPort = scroll.GetComponentInChildren<CanvasRenderer>();
+            _content = viewPort.GetComponentInChildren<ContentSizeFitter>().gameObject;
         }
 
+        private void AddAllListeners()
+        {
+            _hideMenuToggle.onValueChanged.AddListener(delegate { SwitchShow();});
+            _selectAllToggle.onValueChanged.AddListener(delegate { SelectAll();});
+            _hideObjectsToggle.onValueChanged.AddListener(delegate { HideObjects();});
+            _transparency10.onClick.AddListener(delegate { SetAlphaSelected(0.10f);});
+            _transparency30.onClick.AddListener(delegate { SetAlphaSelected(0.30f);});
+            _transparency50.onClick.AddListener(delegate { SetAlphaSelected(0.50f);});
+            _transparency75.onClick.AddListener(delegate { SetAlphaSelected(0.75f);});
+            _transparency100.onClick.AddListener(delegate { SetAlphaSelected(1.00f);});
+        }
     
         private void SwitchShow()
         {
-            var vector = rectTransform.anchoredPosition;
+            var vector = _rectTransform.anchoredPosition;
             vector.x = -vector.x;
-            rectTransform.anchoredPosition = vector;
+            _rectTransform.anchoredPosition = vector;
+        }
+
+        private void SelectAll()
+        {
+            foreach (var objectMenu in _listObjects)
+            {
+                objectMenu.SetSelected(_selectAllToggle.isOn);
+            }
+        }
+
+        private void HideObjects()
+        {
+            foreach (var objectMenu in _listObjects)
+            {
+                if (objectMenu.Selected)
+                    objectMenu.SetShowed(_hideObjectsToggle.isOn);
+            }
+        }
+
+        private void SetAlphaSelected(float value)
+        {
+            foreach (var objectMenu in _listObjects)
+            {
+                if (objectMenu.Selected)
+                    objectMenu.SetAlpha(value);
+            }
         }
     }
 }
--- Assets/RelatedInterface/Scripts/ObjectMenu.cs
+++ Assets/RelatedInterface/Scripts/ObjectMenu.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,41 +8,53 @@ namespace RelatedInterface.Scripts
     public class ObjectMenu : MonoBehaviour
     {
         [SerializeField] private GameObject someObject;
+        [SerializeField] private GameObject prefab;
         
         private TMP_Text _nameObject;
         private Toggle _selected;
         private Toggle _showed;
 
         public bool Selected => _selected.isOn;
+        public GameObject Prefab => prefab;
 
         public void Awake()
         {
             _nameObject = GetComponentInChildren<TMP_Text>();
-            _showed = GetComponentInChildren<Toggle>();
-            _selected = GetComponentInChildren<Toggle>();
+            var toggles = GetComponentsInChildren<Toggle>();
+            _showed = toggles[0];
+            _selected = toggles[1];
             
             _nameObject.text = someObject.name;
             _showed.onValueChanged.AddListener(delegate {SwitchShow();});
         }
 
-        public void SetAlpha(byte value)
+        public void SetAlpha(float value)
         {
-            
+            if (value is < 0 or > 1)
+                throw new ArgumentException("Invalid alpha format");
+
+            if (someObject.TryGetComponent(out Renderer component))
+            {
+                var material = component.material;
+                var color = material.color;
+                color.a = value;
+                material.color = color;
+            }
         }
 
         public void SetSelected(bool value)
         {
-            
+            _selected.isOn = value;
         }
 
         public void SetShowed(bool value)
         {
-            
+            _showed.isOn = value;
         }
 
         private void SwitchShow()
         {
-            someObject.SetActive(_selected.isOn);
+            someObject.SetActive(_showed.isOn);
         }
     }
 }
